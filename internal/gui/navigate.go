87ada679fe1160775b2bdde9dbcfd7a3caaab40e
IdlePhysicist <idlephysicist@vivaldi.net>
2020-09-15 19:02:16 +0100

    updated navigation to incl. inspections

--- internal/gui/navigate.go
+++ internal/gui/navigate.go
@@ -11,9 +11,9 @@ func newNavigate() *navigate {
 	navi := &navigate{
 		TextView: tview.NewTextView().SetTextColor(tview.Styles.PrimaryTextColor),
 		keybindings: map[string]string{
-			"trips" : " n: New Log Entry, m: Modify Log,  d: Remove Log, /: Filter, Enter: Inspect Detail ",
-			"caves" : " n: New Cave, m: Modify Cave, d: Remove Cave, /: Filter ",
-			"cavers": " n: New Caver, m: Modify Caver, d: Remove Caver, /: Filter ",
+			"trips" : " n: New Trip, m: Modify Trip,  d: Remove Trip, /: Filter, Enter: Inspect Detail ",
+			"caves" : " n: New Cave, m: Modify Cave, d: Remove Cave, /: Filter, Enter: Inspect Detail ",
+			"cavers": " n: New Caver, m: Modify Caver, d: Remove Caver, /: Filter, Enter: Inspect Detail ",
 			"detail": " q | ESC: Exit Detail ",
 		},
 	}
